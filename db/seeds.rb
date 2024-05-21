902c8de6407d0537c8ff197bb48928be916f55ff
Enoch Hsu <1529929+ewoknock@users.noreply.github.com>
2024-05-21 09:57:04 -0700

    Creates character model and seeds the data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,17 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+characters = [
+  {name: "Washington Carver", x:0.0797, y:0.6545}, 
+  {name: "Curie", x:0.3016, y:0.7684}, 
+  {name: "Einstein", x:0.1411, y:0.3976}, 
+  {name: "Newton", x:0.9141, y:0.8851}, 
+  {name: "Ride", x:0.5839, y:0.3552}
+]
+
+Character.delete_all
+
+characters.each do |character|
+  Character.create(character)
+end
