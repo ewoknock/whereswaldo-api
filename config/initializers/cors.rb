2fc280e48bf7db56e17bcddc1e486f151e315356
Enoch Hsu <1529929+ewoknock@users.noreply.github.com>
2024-06-09 11:21:15 -0700

    Reverts back to no authenticity token

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,8 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins "http://localhost:5173",
-            "http://127.0.0.1:5173"
+            "http://127.0.0.1:5173",
+            "https://ewoknock.github.io/whereswaldo"
     resource "*",
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
@@ -9,3 +10,8 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
       max_age: 86400
   end
 end
+
+Rails.application.config.action_controller.forgery_protection_origin_check = false
+
+
+
